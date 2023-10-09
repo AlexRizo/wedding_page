15db41635b2cfec73a04c7025dde4835649133f1
Alex <david_alejandrorizo@outlook.com>
2023-10-09 15:09:45 -0600

    select layout

--- public/js/home/form.js
+++ public/js/home/form.js
@@ -314,7 +314,46 @@
                     <button type="submit" class="btn btn-dark btn-save">Guardar</button>
                 </div>
             `,
-            7: `<h1>Selecciona una invitación:</h1>`
+            7: `
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <img src="/assets/img/invitacion.jpg" class="card-img-top" alt="invitación">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="/assets/img/invitacion.jpg" class="card-img-top" alt="invitación">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="/assets/img/invitacion.jpg" class="card-img-top" alt="invitación">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="/assets/img/invitacion.jpg" class="card-img-top" alt="invitación">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `,
         };
         
         switch (step) {
